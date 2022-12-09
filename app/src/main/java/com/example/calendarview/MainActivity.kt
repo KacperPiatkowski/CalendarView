0379287b825d6622f1c4b8ac4369f2b41afd953c
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-12-09 15:41:59 +0100

    zdefiniowanie daty

--- app/src/main/java/com/example/calendarview/MainActivity.kt
+++ app/src/main/java/com/example/calendarview/MainActivity.kt
@@ -10,10 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var kalendarz = findViewById<CalendarView>(R.id.kalendarzView);
+        var WyjazdPrzycisk = findViewById<Button>(R.id.buttonWyjazd);
+        var PowrotPrzycisk = findViewById<Button>(R.id.buttonPowrot);
+        var WyjazdView = findViewById<TextView>(R.id.textViewWyjazd);
+        var PowrotView = findViewById<TextView>(R.id.textViewPowrot);
+
+        var data = arrayListOf<Int>(0,0,0)
+
     }
-    var kalendarz = findViewById<CalendarView>(R.id.kalendarzView);
-    var WyjazdPrzycisk = findViewById<Button>(R.id.buttonWyjazd);
-    var PowrotPrzycisk = findViewById<Button>(R.id.buttonPowrot);
-    var WyjazdView = findViewById<TextView>(R.id.textViewWyjazd);
-    var PowrotView = findViewById<TextView>(R.id.textViewPowrot);
 }
