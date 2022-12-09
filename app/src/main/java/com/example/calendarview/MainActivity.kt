845ef43d6cfb268ec908b5d546c69561c3f33125
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-12-09 15:19:24 +0100

    dodanie zmiennych do Wyjazdu i Powrotu

--- app/src/main/java/com/example/calendarview/MainActivity.kt
+++ app/src/main/java/com/example/calendarview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.calendarview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    var kalendarz = findViewById<CalendarView>(R.id.kalendarzView);
+    var WyjazdPrzycisk = findViewById<Button>(R.id.buttonWyjazd);
+    var PowrotPrzycisk = findViewById<Button>(R.id.buttonPowrot);
+    var WyjazdView = findViewById<TextView>(R.id.textViewWyjazd);
+    var PowrotView = findViewById<TextView>(R.id.textViewPowrot);
 }
